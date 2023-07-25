29794d9339078fce23661bba4c9284806f5b270c
sitraka <sitraka@makoa.fr>
2023-07-25 09:02:02 +0200

    dynamiser home block poser question

--- src/Plugin/Block/AskingQuestionHomeBlock.php
+++ src/Plugin/Block/AskingQuestionHomeBlock.php
@@ -36,6 +36,38 @@ class AskingQuestionHomeBlock  extends BlockBase  {
     $current_user_name = $current_user->getAccountName();
     $service_block = \Drupal::service('phenix_custom_block.view_services');
 
+    $entityTypeManager = \Drupal::entityTypeManager();
+
+    // Load the webform entity by its ID.
+    $webform = $entityTypeManager->getStorage('webform')->load('poser_une_question');
+    // Get the webform's elements, which include the fields.
+    $elements = $webform->getElementsInitialized();
+    $options = $elements['civicrm_1_activity_1_fieldset_fieldset']['civicrm_1_activity_1_cg30_custom_166']['#options'];
+
+    $res_option = [];
+    $is_int = 0;
+    foreach ($options as $index => $option) {
+
+      if (is_string($index)) {
+        $is_int = $index;
+      }
+      
+      if (is_int($index) && $is_int == 'crm_optgroup_social') {
+        $res_option['Droit du travail - Convention Collective - Emploi/Formation'][$index] = $option; 
+      }
+      
+      if (is_int($index) && $is_int == 'crm_optgroup_autres') {
+        $res_option['Autres sujets'][$index] = $option; 
+      }
+    }
+    
+
+    $build['category'] = [
+      '#type' => 'select',
+      '#title' => t('Catégorie'),
+      '#options' => $res_option
+    ];
+
     $data = [];
 
     return [
@@ -43,6 +75,7 @@ class AskingQuestionHomeBlock  extends BlockBase  {
       '#cache' => ['max-age' => 0],
       '#content' => [
         'data' => $data,
+        'option' => $build,
       ],
     ];
   }
