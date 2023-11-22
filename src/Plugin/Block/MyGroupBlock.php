f451b26c0d3ee39f4970d3fd8fc10d3143ba72c8
sitraka <sitraka@makoa.fr>
2023-11-22 08:01:51 +0100

    par defaut assigner à Myriam,  creer une activité pour poser question, ajout doc renommer social en Verouillé social

--- src/Plugin/Block/MyGroupBlock.php
+++ src/Plugin/Block/MyGroupBlock.php
@@ -43,6 +43,7 @@ class MyGroupBlock  extends BlockBase  {
     $results = $service_block->getAllMyGroup($cid);
      
     $data = [];
+    
     foreach ($results as $res) {
       $total_membre = $service_block->getTotalGroupMember($res->civicrm_group_civicrm_group_contact_id);
       $total_membre = $total_membre[0]['count'];
@@ -54,13 +55,71 @@ class MyGroupBlock  extends BlockBase  {
       ];
     }
 
+    // Create a new DrupalDateTime object with the current date and time.
+    $current_date = new \Drupal\Core\Datetime\DrupalDateTime('now');
+
+    // You can format the date as needed.
+    $formatted_date = $current_date->format('Y-m-d H:i:s');
+    $allGroupes = [];
+    $newData = [];
+    foreach ($data as $group) {
+      $groupId = $group['group_id'];
+      $events = \Civi\Api4\Event::get(FALSE)
+        ->addSelect('title', 'start_date')
+        ->addWhere('rsvpevent_cg_linked_groups.rsvpevent_cf_linked_groups', '=', $groupId)
+        ->addWhere('start_date', '>=', $formatted_date)
+        ->addOrderBy('start_date', 'DESC')
+        ->execute();
+
+        $groupContacts = \Civi\Api4\GroupContact::get(FALSE)
+          ->addSelect('COUNT(contact_id) AS count', 'group_id.title')
+          ->addWhere('group_id', '=', $groupId)
+          ->execute()->first();
+        $total = $groupContacts['count'];
+        $total_multiple = $total > 1 ? 'Personnes' : 'Personne';
+        $gtitle = $groupContacts['group_id.title'];
+        $writable = &$events->first();
+        $writable['group_id'] = ['data' => 
+        [
+              'title' => $gtitle,
+              'total' => $total,
+              'total_multiple' => $total_multiple,
+              'group_id' => $groupId
+            ]
+        ];
+        $allGroupes[] = $writable ;
+      //   $allGroupes[] = [$events->first() => [$groupId, 'data' => [
+      //     'title' => $gtitle,
+      //     'total' => $total,
+      //     'total_multiple' => $total_multiple,
+      //     'group_id' => $groupId
+      //   ]
+      // ]];
+        // $allGroupes[] = ['data' => $gtitle];
+    }
+    // Sort the array using the custom comparison function.
+    usort($allGroupes, [$this, 'sortByStartDate'] );
+
+    $result = [];
+    foreach ($allGroupes as $item) {
+      // dump($item, ' kkk');
+        $data = $item['group_id']['data'];
+        $result[] = $data;
+    }
+    
+
     return [
       '#theme' => 'my_group',
       '#cache' => ['max-age' => 0],
       '#content' => [
-        'data' => $data,
+        'data' => $result,
       ],
     ];
   }
+
+  // Define a custom comparison function for sorting by "start_date".
+public function sortByStartDate($a, $b) {
+  return strtotime($a['start_date']) - strtotime($b['start_date']);
+}
 
 }
