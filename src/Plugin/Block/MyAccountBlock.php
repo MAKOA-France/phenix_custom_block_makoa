d380450870cf2ca02ada721d95eb77a7367716ac
sitraka <sitraka@makoa.fr>
2024-01-09 06:29:58 +0100

    user anonyme , ne pas afficher le block mon compte

--- src/Plugin/Block/MyAccountBlock.php
+++ src/Plugin/Block/MyAccountBlock.php
@@ -31,56 +31,59 @@ class MyAccountBlock  extends BlockBase  {
 
     // Get the current user.
     $current_user = \Drupal::currentUser();
+    if ($current_user->id()) {
 
-    // Get the current user's name.
-    $current_user_name = $current_user->getAccountName();
+
+      // Get the current user's name.
+      $current_user_name = $current_user->getAccountName();
+      
+      $menu_name = 'account';
+      $depth = 3;
     
-    $menu_name = 'account';
-    $depth = 3;
-  
-    /** @var \Drupal\Core\Menu\MenuLinkTreeInterface $menuLinkTree */
-    $menuLinkTree = \Drupal::service('menu.link_tree');
-    $parameters = new MenuTreeParameters();
-    $parameters->setMaxDepth($depth);
-    $tree = $menuLinkTree->load($menu_name, $parameters);
-    $manipulators = [
-      ['callable' => 'menu.default_tree_manipulators:checkAccess'],
-      ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
-    ];
-    $tree = $menuLinkTree->transform($tree, $manipulators);
-    $menu_tree = $menuLinkTree->build($tree);
-  
-    $menu_items = [];
-  
-    // Get the current path.
-    $currentPath = \Drupal::service('path.current')->getPath();
+      /** @var \Drupal\Core\Menu\MenuLinkTreeInterface $menuLinkTree */
+      $menuLinkTree = \Drupal::service('menu.link_tree');
+      $parameters = new MenuTreeParameters();
+      $parameters->setMaxDepth($depth);
+      $tree = $menuLinkTree->load($menu_name, $parameters);
+      $manipulators = [
+        ['callable' => 'menu.default_tree_manipulators:checkAccess'],
+        ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
+      ];
+      $tree = $menuLinkTree->transform($tree, $manipulators);
+      $menu_tree = $menuLinkTree->build($tree);
     
-    $all_menu = [];
-    foreach ($menu_tree as $key => $item) {
-      if (isset($item['user.page'])) {
-        $keys = array_keys($item);
-        for ($i=0; $i<count($keys); $i++) {
-          $title = is_string($item[$keys[$i]]['title']) ? $item[$keys[$i]]['title'] :  $item[$keys[$i]]['title']->__toString();
-          $all_menu[] = ['url' => $item[$keys[$i]]['url']->toString(), 'title' => $title];
+      $menu_items = [];
+    
+      // Get the current path.
+      $currentPath = \Drupal::service('path.current')->getPath();
+      
+      $all_menu = [];
+      foreach ($menu_tree as $key => $item) {
+        if (isset($item['user.page'])) {
+          $keys = array_keys($item);
+          for ($i=0; $i<count($keys); $i++) {
+            $title = is_string($item[$keys[$i]]['title']) ? $item[$keys[$i]]['title'] :  $item[$keys[$i]]['title']->__toString();
+            $all_menu[] = ['url' => $item[$keys[$i]]['url']->toString(), 'title' => $title];
+          }
         }
       }
+
+      // $all_menu[0]['icon'] = "fas fa-comment";
+      $all_menu[0]['icon'] = "fas fa-home";
+      $all_menu[1]['icon'] = 'icon-custom-user-account';
+      $all_menu[2]['icon'] = "icon-custom-logout-accout";
+      $all_menu[3]['icon'] = 'icon-custom-refresh';
+  
+
+      return [
+        '#theme' => 'my_account_block',
+        '#cache' => ['max-age' => 0],
+        '#content' => [
+          'username' => $current_user_name,
+          'menu_item' => $all_menu,
+        ],
+      ];
     }
-
-    // $all_menu[0]['icon'] = "fas fa-comment";
-    $all_menu[0]['icon'] = "fas fa-home";
-    $all_menu[1]['icon'] = 'icon-custom-user-account';
-    $all_menu[2]['icon'] = "icon-custom-logout-accout";
-    $all_menu[3]['icon'] = 'icon-custom-refresh';
- 
-
-    return [
-      '#theme' => 'my_account_block',
-      '#cache' => ['max-age' => 0],
-      '#content' => [
-        'username' => $current_user_name,
-        'menu_item' => $all_menu,
-      ],
-    ];
   }
 
 }
